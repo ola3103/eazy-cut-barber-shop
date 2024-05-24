8dac77e38cfa5174ea073d9027b64020cecab9ec
ola3103 <wonuola.og31@yahoo.com>
2024-05-24 18:06:28 +0100

    email bug fixed

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -6,6 +6,9 @@ const sendEmail = async ({ to, html, subject }) => {
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
     secure: false,
+    tls: {
+      ciphers: "SSLv3",
+    },
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
