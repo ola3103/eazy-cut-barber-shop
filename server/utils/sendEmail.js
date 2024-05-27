4f30d7e264955e37236f8dc35843ef4baa8db293
ola3103 <wonuola.og31@yahoo.com>
2024-05-27 23:58:20 +0100

    email bug fixed 006

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -3,6 +3,7 @@
 
 // const sendEmail = async ({ to, html, subject }) => {
 //   const transporter = nodemailer.createTransport({
+
 //     host: process.env.EMAIL_HOST,
 //     port: process.env.EMAIL_PORT,
 //     secure: false,
@@ -14,51 +15,39 @@
 
 //   const mailOptions = { from: process.env.EMAIL_USER, to, subject, html };
 
-//   // transporter.sendMail(mailOptions, function (err, info) {
-//   //   if (err) {
-//   //     console.log(err);
-//   //     throw new CustomError("Error sending email", 500);
-//   //   }
-//   // });
-
-//   await new Promise((resolve, reject) => {
-//     transporter.sendMail(mailOptions, (err, info) => {
-//       if (err) {
-//         console.error(err);
-//         reject(err);
-//       } else {
-//         resolve(info);
-//       }
-//     });
+//   transporter.sendMail(mailOptions, function (err, info) {
+//     if (err) {
+//       console.log(err);
+//       throw new CustomError("Error sending email", 500);
+//     }
 //   });
 // };
 
 // module.exports = sendEmail;
 
-const SibApiV3Sdk = require("sib-api-v3-sdk");
+const nodemailer = require("nodemailer");
+const CustomError = require("../errors/customError");
 
 const sendEmail = async ({ to, html, subject }) => {
-  const defaultClient = SibApiV3Sdk.ApiClient.instance;
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS,
+    },
+  });
 
-  let apiKey = defaultClient.authentications["api-key"];
-  apiKey.apiKey = process.env.BREVO_API_KEY;
+  const mailOptions = { from: process.env.EMAIL_USER, to, subject, html };
 
-  const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
-
-  const sender = { name: "Eazy Cutz", email: process.env.EMAIL_USER };
-  const receivers = [{ email: to }];
-
-  try {
-    const sendEmail = await apiInstance.sendTransacEmail({
-      sender,
-      to: receivers,
-      subject,
-      htmlContent: html,
-    });
-    console.log(sendEmail);
-  } catch (error) {
-    console.log(error);
-  }
+  transporter.sendMail(mailOptions, function (err, info) {
+    if (err) {
+      console.log(err);
+      throw new CustomError("Error sending email", 500);
+    }
+  });
 };
 
 module.exports = sendEmail;
