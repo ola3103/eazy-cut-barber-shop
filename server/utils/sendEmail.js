3f7cda1c8183839f24184ecf3ca9a80b1f6e8c4a
ola3103 <wonuola.og31@yahoo.com>
2024-05-29 00:20:40 +0100

    email bug fixed 018

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -34,30 +34,73 @@
 
 // module.exports = sendEmail;
 
+// const nodemailer = require("nodemailer");
+// const CustomError = require("../errors/customError");
+
+// const sendEmail = async ({ to, html, subject }) => {
+//   const transporter = nodemailer.createTransport({
+//     host: "smtp-mail.outlook.com",
+//     port: 587,
+//     secure: false,
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS,
+//     },
+//     tls: {
+//       rejectUnauthorized: false,
+//     },
+//   });
+
+//   const mailOptions = { from: process.env.EMAIL_USER, to, subject, html };
+
+//   try {
+//     await transporter.sendMail(mailOptions);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// module.exports = sendEmail;
+
 const nodemailer = require("nodemailer");
-const CustomError = require("../errors/customError");
 
-const sendEmail = async ({ to, html, subject }) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp-mail.outlook.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-    tls: {
-      rejectUnauthorized: false,
-    },
+const wrappedSendMail = async (mailOptions) => {
+  return new Promise((resolve, reject) => {
+    let transporter = nodemailer.createTransport({
+      host: "smtp-mail.outlook.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+        resolve(false);
+      } else {
+        console.log(`email sent: ${info.response}`);
+        resolve(true);
+      }
+    });
   });
+};
 
-  const mailOptions = { from: process.env.EMAIL_USER, to, subject, html };
+const sendEmail = async ({ to, subject, html }) => {
+  let mailOptions = {
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    html,
+  };
 
-  try {
-    await transporter.sendMail(mailOptions);
-  } catch (error) {
-    console.log(error);
-  }
+  let resp = await wrappedSendMail(mailOptions);
+  return resp;
 };
 
 module.exports = sendEmail;
