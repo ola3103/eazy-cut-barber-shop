46c854e55ede18f7eb15aa4eac286695896f820b
ola3103 <wonuola.og31@yahoo.com>
2024-05-28 12:57:21 +0100

    email bug fixed 013

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -48,33 +48,9 @@ const sendEmail = async ({ to, html, subject }) => {
     },
   });
 
-  await new Promise((resolve, reject) => {
-    // verify connection configuration
-    transporter.verify(function (error, success) {
-      if (error) {
-        console.log(error);
-        reject(error);
-      } else {
-        console.log("Server is ready to take our messages");
-        resolve(success);
-      }
-    });
-  });
-
   const mailOptions = { from: process.env.EMAIL_USER, to, subject, html };
 
-  await new Promise((resolve, reject) => {
-    // send mail
-    transporter.sendMail(mailOptions, (err, info) => {
-      if (err) {
-        console.error(err);
-        reject(err);
-      } else {
-        console.log(info);
-        resolve(info);
-      }
-    });
-  });
+  await transporter.sendMail(mailOptions);
 };
 
 module.exports = sendEmail;
