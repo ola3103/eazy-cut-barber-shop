5efa88d177a7d82f58462e0870d5a27765b6f985
ola3103 <wonuola.og31@yahoo.com>
2024-05-28 12:49:54 +0100

    email bug fixed 012

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,56 +1,30 @@
-const nodemailer = require("nodemailer");
-const CustomError = require("../errors/customError");
-
-const sendEmail = async ({ to, html, subject }) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: false,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-    authMethod: "PLAIN",
-    tls: {
-      rejectUnauthorized: false,
-    },
-  });
-
-  const mailOptions = { from: process.env.EMAIL_USER, to, subject, html };
-
-  // transporter.sendMail(mailOptions, function (err, info) {
-  //   if (err) {
-  //     console.log(err);
-  //     throw new CustomError("Error sending email", 500);
-  //   }
-  // });
-
-  try {
-    await transporter.sendMail(mailOptions);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-module.exports = sendEmail;
-
 // const nodemailer = require("nodemailer");
 // const CustomError = require("../errors/customError");
 
 // const sendEmail = async ({ to, html, subject }) => {
 //   const transporter = nodemailer.createTransport({
-//     service: "Gmail",
-//     host: "smtp.gmail.com",
-//     port: 465,
-//     secure: true,
+//     host: process.env.EMAIL_HOST,
+//     port: process.env.EMAIL_PORT,
+//     secure: false,
 //     auth: {
-//       user: process.env.GMAIL_USER,
-//       pass: process.env.GMAIL_PASS,
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS,
 //     },
-//     authMethod: "LOGIN",
+//     authMethod: "PLAIN",
+//     tls: {
+//       rejectUnauthorized: false,
+//     },
 //   });
 
 //   const mailOptions = { from: process.env.EMAIL_USER, to, subject, html };
+
+//   // transporter.sendMail(mailOptions, function (err, info) {
+//   //   if (err) {
+//   //     console.log(err);
+//   //     throw new CustomError("Error sending email", 500);
+//   //   }
+//   // });
+
 //   try {
 //     await transporter.sendMail(mailOptions);
 //   } catch (error) {
@@ -59,3 +33,48 @@ module.exports = sendEmail;
 // };
 
 // module.exports = sendEmail;
+
+const nodemailer = require("nodemailer");
+const CustomError = require("../errors/customError");
+
+const sendEmail = async ({ to, html, subject }) => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp-mail.outlook.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        console.log("Server is ready to take our messages");
+        resolve(success);
+      }
+    });
+  });
+
+  const mailOptions = { from: process.env.EMAIL_USER, to, subject, html };
+
+  await new Promise((resolve, reject) => {
+    // send mail
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else {
+        console.log(info);
+        resolve(info);
+      }
+    });
+  });
+};
+
+module.exports = sendEmail;
